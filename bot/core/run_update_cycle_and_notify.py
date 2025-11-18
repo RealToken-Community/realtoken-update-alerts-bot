67bc80c858d22da6ce4eb4a4468e8422457c56eb
Logan Sulpizio <logan.sulpizio@widetech.com>
2025-11-18 17:19:07 +0100

    improve logic when no update detected

--- bot/core/run_update_cycle_and_notify.py
+++ bot/core/run_update_cycle_and_notify.py
@@ -31,29 +31,32 @@ async def run_update_cycle_and_notify(app: Application) -> None:
     realtoken_history_data_last = app.bot_data["realtoken_history"]
     realtoken_history_state_last = app.bot_data["realtoken_history_state"]
     realtoken_history_data_current = list_to_dict_by_uuid(fetch_json(REALTOKEN_HISTORY_URL))
+    
+    # If API not available or parsing failed, stop the cycle gracefully
+    if realtoken_history_data_current is None:
+        logger.warning("Realtoken history data not fetched (API might be unavailable). Skipping update cycle.")
+        return
+    
     realtoken_history_state_current = build_history_state(realtoken_history_data_current)
-
-    ### get new updates of realtoken history from community API ###
-
     new_history_items_by_uuid = get_new_updates(app, realtoken_history_data_current, realtoken_history_state_last, realtoken_history_state_current, realtoken_data)
-
-    # Loop over user IDs and preferences and filter messages
-    for user_id, prefs in user_manager.users.items():
-
-        lines_messages = build_lines_messages(new_history_items_by_uuid, realtoken_data, realtoken_history_data_last, user_manager, i18n, user_id)
-
-        message = filter_messages(lines_messages, user_id, prefs.notification_types, prefs.token_scope)
-
-        if message and message.strip(): # ensures the string has at least one non-whitespace character
-            await app.bot.send_message(
-                    chat_id=user_id,
-                    text = re.sub(r'([.\-()])', r'\\\1', message),
-                    parse_mode=ParseMode.MARKDOWN_V2
-                )
+     
+    # Loop over user IDs and preferences and filter messages if there is at least a new item
+    if len(new_history_items_by_uuid) > 0:
+        for user_id, prefs in user_manager.users.items():
+    
+            lines_messages = build_lines_messages(new_history_items_by_uuid, realtoken_data, realtoken_history_data_last, user_manager, i18n, user_id)
+    
+            message = filter_messages(lines_messages, user_id, prefs.notification_types, prefs.token_scope)
+    
+            if message and message.strip(): # ensures the string has at least one non-whitespace character
+                await app.bot.send_message(
+                        chat_id=user_id,
+                        text = re.sub(r'([.\-()])', r'\\\1', message),
+                        parse_mode=ParseMode.MARKDOWN_V2
+                    )
 
     # update new realtoken history
     app.bot_data["realtoken_history_state"] = realtoken_history_state_current
     app.bot_data["realtoken_history"] = realtoken_history_data_current
 
     logger.info(f"Update cycle completed: {len(new_history_items_by_uuid)} tokens updated")
-    
